566f87f439bcfe047785a9115a99982fd8509c69
jcbcouch <jcbcouch@gmail.com>
2023-10-04 21:36:46 -0500

    init

---
+++ src/main/java/com/jacobcouch/coolspringapp/CoolspringappApplication.java
@@ -0,0 +1,13 @@
+package com.jacobcouch.coolspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolspringappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoolspringappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jacobcouch/coolspringapp/CoolspringappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacobcouch.coolspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolspringappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
